3fb9cb0c34197be8bd5b33e53ec51bb6cdca243d
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-11-24 15:07:24 +0530

    test

--- src/util.js
+++ src/util.js
@@ -2,8 +2,4 @@ const randomNumberGenerator = function(){
   return Math.ceil(Math.random()*9);
 }
 
-const deleteElement  = function(array,index) {
-  return array.splice(index,0);
-}
-
-module.exports = { randomNumberGenerator,deleteElement }; 
+module.exports = { randomNumberGenerator }; 
