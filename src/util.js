aa9de8a607a991bc82742a68c0d0a5604f9131cd
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-11-24 16:15:50 +0530

    Introduced isWithin1to9 and isValid functions in src/utlil.js

--- src/util.js
+++ src/util.js
@@ -7,4 +7,12 @@ const deleteElement  = function(array,index) {
   return array;
 }
 
-module.exports = { randomNumberGenerator,deleteElement }; 
+const isWithin1to9 = function(numberToCheck){
+  return numberToCheck >= 1 && numberToCheck < 10;
+}
+
+const isValid = function(moves,moveToCheck){
+  return isWithin1to9(moveToCheck) && moves.includes(moveToCheck)
+}
+
+module.exports = { randomNumberGenerator,deleteElement,isWithin1to10,isValid }; 
